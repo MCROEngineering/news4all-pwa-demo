9cf1dbb912974560c4d6e01710424ecc0fe08027
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-01-21 18:59:32 +0200

    - Add splash and app icon (not fully completed though) - Add favicon - Replace superagent with axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,18 @@
-import request from 'superagent';
+import axios from 'axios'
 
 export const getAllNewsRequest = () => {
   return new Promise((resolve, reject) => {
-    request
+
+    axios
       .get(`https://newsapi.org/v2/everything?q=bitcoin&from=2018-12-21&sortBy=publishedAt&apiKey=0e70989b27d641a6af2082bcbd12889b`)
-      .end((err, res) => {
-        if (res.body.success !== false) {
-          return resolve(res.body);
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('Res: ', res);
+          return resolve(res.data);
         }
+      })
+      .catch(err => {
         reject(err);
-      });
+      })
   });
 };

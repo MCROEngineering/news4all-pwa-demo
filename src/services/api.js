87761ad41ee5cb878a0bfb49d8756dfc4239d22c
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-01-22 13:54:46 +0200

    Get news from WSJ

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const getAllNewsRequest = () => {
   return new Promise((resolve, reject) => {
     axios
-      .get(`https://newsapi.org/v2/everything?q=bitcoin&from=2018-12-22&sortBy=publishedAt&apiKey=0e70989b27d641a6af2082bcbd12889b`)
+      .get(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=0e70989b27d641a6af2082bcbd12889b`)
       .then((res) => {
         if (res.status === 200) {
           setTimeout(() => {
